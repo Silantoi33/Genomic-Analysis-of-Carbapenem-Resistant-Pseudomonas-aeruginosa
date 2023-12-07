6bc2f487551f6bc954804757831aefeecff2ef05
mkevinkariuki <140628205+mkevinkariuki@users.noreply.github.com>
2023-12-07 10:11:14 +0300

    Create bash script to identify ST
    For loop bash script to identify isolate MLST

---
+++ Scripts/gacrpa_mlst.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Authors: Silantoi & Kevin
+#Date: 27/11/2023
+#Modified Date: 07/12/2023
+Script name:gacrpa_mlst.sh
+#Usage: bash script name
+#Description: For loop bash script to identify the multi locus sequence type (MLST)
+
+# do mlst
+for a in *.fa
+do
+mlst $a | cut -f 1,2,3 >> /home/sequser/Downloads/GACRPA-Project/gacrpa_results/gacrpa_mlst.tsv
+done
+
+for b in *.fsa_nt
+do
+mlst $b | cut -f 1,2,3 >> /home/sequser/Downloads/GACRPA-Project/gacrpa_results/gacrpa_mlst.tsv
+done
